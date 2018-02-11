ea125e5608aaa1b85a2b03c7aee8b60eabcd546d
ncentola <ncentola@iloan.com>
2018-02-11 11:49:10 -0600

    will set up new virtualenv with necessary packages

---
+++ container_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  VIRTUALENV_NAME='lendingclub_analysis'
+else
+  VIRTUALENV_NAME=$1
+fi
+
+virtualenv --system-site-packages -p python3 ~/$VIRTUALENV_NAME
+source ~/$VIRTUALENV_NAME/bin/activate
+
+pip install .
+pip install -r requirements.txt
